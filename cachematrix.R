7a767a28fe42b9167ed52061bfc48992f279f30c
jeevanrag <ragjeevan@gmail.com>
2016-01-24 16:29:30 -0500

    CacheMatrix Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,25 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+ cached_inverse <<- solve(x)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+		if (exists("cached_inverse"))
+		{
+			print ("cached inverse")
+			return(cached_inverse)
+		}
+		else
+		{
+			print("non-cached inverse") 
+			solve(x)
+		}
 }
